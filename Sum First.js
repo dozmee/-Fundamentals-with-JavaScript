56a1daa37577f883e53956288adf7399c22d0acf
Vladimir Danev <130492081+dozmee@users.noreply.github.com>
2024-01-14 09:38:18 +0200

    Create Sum First.js

---
+++ Sum First.js
@@ -0,0 +1,13 @@
+function demo(input){
+
+
+let furstNum = input[0]
+let lastNum = input[input.length - 1]
+
+let sum = furstNum + lastNum
+console.log(sum);
+
+
+}
+demo([20,30,40])
+demo([10,17,22,33])
