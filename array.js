052ff8deb76e78e658456d9fbc3c53f159b84e4c
Vladimir Danev <130492081+dozmee@users.noreply.github.com>
2024-05-12 19:58:09 +0300

    Create array.js

---
+++ array.js
@@ -0,0 +1,9 @@
+function array(input) {
+
+    let furstArray = input[0];
+    let lastArray = input[input.length - 1]
+    let sum = furstArray + lastArray
+    console.log(sum);
+
+}
+array([20, 30, 40])
