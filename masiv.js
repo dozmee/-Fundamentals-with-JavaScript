3e50a3d835b3d98737a36574c5fa3a352697d85e
Vladimir Danev <130492081+dozmee@users.noreply.github.com>
2024-05-12 19:58:17 +0300

    Create masiv.js

---
+++ masiv.js
@@ -0,0 +1,12 @@
+function masiv(input) {
+
+    let duljina = input[input.length - 1]
+    console.log(duljina);
+
+
+    let count = input.length
+    let dulyg = input[count - 1]
+    console.log(dulyg);
+
+}
+masiv([11, 33, 44, 55, 22, 44,])
